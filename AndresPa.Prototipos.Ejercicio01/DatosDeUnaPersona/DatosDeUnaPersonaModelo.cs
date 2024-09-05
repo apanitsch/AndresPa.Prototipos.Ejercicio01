e831ecf84d936895582daf503e5b766aaeaeeaf3
apanitsch <apanitsch@gmail.com>
2024-09-05 19:56:56 -0300

    DatosDeUnaPersona - Esqueleto.

---
+++ AndresPa.Prototipos.Ejercicio01/DatosDeUnaPersona/DatosDeUnaPersonaModelo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndresPa.Prototipos.Ejercicio01.DatosDeUnaPersona;
+internal class DatosDeUnaPersonaModelo
+{
+
+
+}
