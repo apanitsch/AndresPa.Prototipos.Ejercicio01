ec9b5542494a1f501362f61cb541b9c06ef25c18
apanitsch <apanitsch@gmail.com>
2024-09-05 19:43:26 -0300

    Un comentario para un push de prueba.

--- AndresPa.Prototipos.Ejercicio01/Program.cs
+++ AndresPa.Prototipos.Ejercicio01/Program.cs
@@ -8,6 +8,8 @@ internal static class Program
     [STAThread]
     static void Main()
     {
+        //Hola esto es un comentario y no sirve para nada.
+
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
